02b0a28c91c0f0de6c7e945aea8c6bb9b2e9ccfb
Charignon Kantin <kantin@digitalgears.fr>
2018-03-16 13:58:26 +0100

    100%

--- Shadows of the Knight - 1/main.php
+++ Shadows of the Knight - 1/main.php
@@ -5,37 +5,39 @@
  **/
 
 fscanf(STDIN, "%d %d",
-    $WT, // width of the building.
-    $HT // height of the building.
+    $LimitRight, // width of the building.
+    $LimitBottom // height of the building.
 );
-$WB = ($HB = 0);
+$LimitLeft = ($LimitTop = 0);
+$LimitRight--;
+$LimitBottom--;
 fscanf(STDIN, "%d",
     $N // maximum number of turns before game over.
 );
 fscanf(STDIN, "%d %d",
-    $XB,
-    $YB
+    $BatmanX,
+    $BatmanY
 );
-
 // game loop
 while (TRUE)
 {
     fscanf(STDIN, "%s",
         $bombDir // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
     );
-    if (strpos($bombDir, 'U') !== false)
-        $HT = ($YB /= 2);
-    else if (strpos($bombDir, 'D') !== false)
-        $YB +=  ($HT - $YB) / 2;
-    if (strpos($bombDir, 'L') !== false)
-        $WT = ($XB /= 2);
-    else if (strpos($bombDir, 'R') !== false)
-        $XB +=  ($WT - $XB) / 2;
-    // Write an action using echo(). DON'T FORGET THE TRAILING \n
-    // To debug (equivalent to var_dump): error_log(var_export($var, true));
-
-
-    // the location of the next window Batman should jump to.
-    echo(intval($XB)." ". intval($YB).PHP_EOL);
+    if (strpos($bombDir, 'U') !== false) {
+        $LimitBottom = $BatmanY;
+        $BatmanY -= ceil(abs($LimitTop - $BatmanY) / 2);
+    } else if (strpos($bombDir, 'D') !== false) {
+        $LimitTop = $BatmanY;
+        $BatmanY += ceil(abs($LimitBottom - $BatmanY) / 2);
+    }
+    if (strpos($bombDir, 'L') !== false) {
+        $LimitRight = $BatmanX;
+        $BatmanX -= ceil(abs($LimitLeft - $BatmanX) / 2);
+    } else if (strpos($bombDir, 'R') !== false) {
+        $LimitLeft = $BatmanX;
+        $BatmanX +=  ceil(abs($LimitRight - $BatmanX) / 2);
+    }
+    echo(intval($BatmanX)." ". intval($BatmanY).PHP_EOL);
 }
 ?>
