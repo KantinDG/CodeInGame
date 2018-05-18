728f8d351fb277020b9a93c2a2116b9ff5e5c0d9
Charignon Kantin <kantin@digitalgears.fr>
2018-05-18 11:34:24 +0200

    Recuperation des donnees

--- La Bataille/main.php
+++ La Bataille/main.php
@@ -7,12 +7,17 @@
 fscanf(STDIN, "%d",
     $n // the number of cards for player 1
 );
+$stackP1 = array();
+$replace = ["11", "12", "13", "14"];
+$search = ["J", "Q", "K", "A"];
 for ($i = 0; $i < $n; $i++)
 {
     fscanf(STDIN, "%s",
         $cardp1 // the n cards of player 1
     );
+    $stackP1[] = intval(str_replace($search, $replace, $cardp1));
 }
+error_log('P1 ['.implode(', ', $stackP1)."]");
 fscanf(STDIN, "%d",
     $m // the number of cards for player 2
 );
@@ -21,7 +26,9 @@ for ($i = 0; $i < $m; $i++)
     fscanf(STDIN, "%s",
         $cardp2 // the m cards of player 2
     );
+    $stackP2[] = intval(str_replace($search, $replace, $cardp2));
 }
+error_log('P2 ['.implode(', ', $stackP2)."]");
 
 // Write an action using echo(). DON'T FORGET THE TRAILING \n
 // To debug (equivalent to var_dump): error_log(var_export($var, true));
